74d48e6d882422ee96edfc35f13d1d7027762f0d
ljskr <ljskr@163.com>
2025-10-31 15:23:21 +0800

    提交 删除config.py中的配置

--- backup.py
+++ backup.py
@@ -6,7 +6,6 @@ import logging.config
 import os
 
 from easybk import TaskManager, UploadManager
-from config import init_task
 from config_parser import init_from_yaml
 
 
@@ -33,6 +32,7 @@ def main():
     
     # 从 Python 配置文件加载（如果存在）
     if os.path.exists("config.py"):
+        from config import init_task
         init_task(task_manager, upload_manager)
     
     task_manager.run_all_task()
--- config.py
+++ config.py
@@ -12,63 +12,64 @@ def init_task(task_manager: TaskManager, upload_manager: UploadManager):
     配置任务信息
     """
 
-    date_str = datetime.datetime.now().strftime("%Y%m%d")
-    output_base_dir = os.path.join("/root/backup/data", date_str)
-    if not os.path.exists(output_base_dir):
-        os.makedirs(output_base_dir, exist_ok=True)
+    # date_str = datetime.datetime.now().strftime("%Y%m%d")
+    # output_base_dir = os.path.join("/root/backup/data", date_str)
+    # if not os.path.exists(output_base_dir):
+    #     os.makedirs(output_base_dir, exist_ok=True)
 
-    # PackTask 示例
-    dokuwiki_task = PackTask(task_name="dokuwiki",
-                             output_dir=os.path.join(output_base_dir, "dokuwiki"),
-                             tar_run_dir="/var/www",
-                             backup_list=["dokuwiki/conf",
-                                          "dokuwiki/data/attic",
-                                          "dokuwiki/data/meta",
-                                          "dokuwiki/data/pages",
-                                          "dokuwiki/data/media",
-                                          "dokuwiki/data/media_attic",
-                                          "dokuwiki/data/media_meta"])
+    # # PackTask 示例
+    # dokuwiki_task = PackTask(task_name="dokuwiki",
+    #                          output_dir=os.path.join(output_base_dir, "dokuwiki"),
+    #                          tar_run_dir="/var/www",
+    #                          backup_list=["dokuwiki/conf",
+    #                                       "dokuwiki/data/attic",
+    #                                       "dokuwiki/data/meta",
+    #                                       "dokuwiki/data/pages",
+    #                                       "dokuwiki/data/media",
+    #                                       "dokuwiki/data/media_attic",
+    #                                       "dokuwiki/data/media_meta"])
 
-    # MysqlTask 示例
-    db1_task = MysqlTask(task_name="db1",
-                         output_dir=os.path.join(output_base_dir, "mydb"),
-                         dump_option="-u root --databases mydb")
+    # # MysqlTask 示例
+    # db1_task = MysqlTask(task_name="db1",
+    #                      output_dir=os.path.join(output_base_dir, "mydb"),
+    #                      dump_option="-u root --databases mydb")
 
-    # SingleFileTask 示例
-    file1_task = SingleFileTask(task_name="file1",
-                                output_dir=os.path.join(output_base_dir, "myfile"),
-                                source_file="/etc/profile")
+    # # SingleFileTask 示例
+    # file1_task = SingleFileTask(task_name="file1",
+    #                             output_dir=os.path.join(output_base_dir, "myfile"),
+    #                             source_file="/etc/profile")
 
-    task_manager.add_task(dokuwiki_task)
-    task_manager.add_task(db1_task)
-    task_manager.add_task(file1_task)
+    # task_manager.add_task(dokuwiki_task)
+    # task_manager.add_task(db1_task)
+    # task_manager.add_task(file1_task)
 
-    # 定义上传器
-    oss_uploader = OSSUploader(name="oss_uploader",
-                               access_id="access_id",
-                               access_key="access_key",
-                               endpoint="endpoint",
-                               bucket_name="bucket_name")
+    # # 定义上传器
+    # oss_uploader = OSSUploader(name="oss_uploader",
+    #                            access_id="access_id",
+    #                            access_key="access_key",
+    #                            endpoint="endpoint",
+    #                            bucket_name="bucket_name")
 
-    ftp_uploader = FTPUploader(name="ftp_uploader",
-                               host="host",
-                               port=21,
-                               username="username",
-                               password="password",
-                               secure=False,
-                               passive=True)
+    # ftp_uploader = FTPUploader(name="ftp_uploader",
+    #                            host="host",
+    #                            port=21,
+    #                            username="username",
+    #                            password="password",
+    #                            secure=False,
+    #                            passive=True)
 
-    # OSS 上传示例
-    oss_upload_task = UploadTask(task=dokuwiki_task,
-                                 uploader=oss_uploader,
-                                 remote_dir=os.path.join(date_str, "dokuwiki"))
-    upload_manager.add_upload_task(oss_upload_task)
+    # # OSS 上传示例
+    # oss_upload_task = UploadTask(task=dokuwiki_task,
+    #                              uploader=oss_uploader,
+    #                              remote_dir=os.path.join(date_str, "dokuwiki"))
+    # upload_manager.add_upload_task(oss_upload_task)
 
-    # FTP 上传示例
-    ftp_upload_task = UploadTask(task=dokuwiki_task,
-                                 uploader=ftp_uploader,
-                                 remote_dir=os.path.join(date_str, "dokuwiki"))
-    upload_manager.add_upload_task(ftp_upload_task)
+    # # FTP 上传示例
+    # ftp_upload_task = UploadTask(task=dokuwiki_task,
+    #                              uploader=ftp_uploader,
+    #                              remote_dir=os.path.join(date_str, "dokuwiki"))
+    # upload_manager.add_upload_task(ftp_upload_task)
 
+    pass
 
 
